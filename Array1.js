af5a8fc62916913feb3dc4a3a145adf00ef5e8fb
YberAldayr <yber.altayr@gmail.com>
2024-01-04 19:00:43 -0400

    agregue Array1, Ejercicio_1 y README.md

---
+++ Array1.js
@@ -0,0 +1,40 @@
+
+/*const cars = ["Saab", "Volvo", "BMW"];
+let x = cars[1];
+console.log(x);
+*/
+
+/*
+//array para determinar tamaño de "fruits"
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let size = fruits.length;
+console.log(size);
+*/
+
+/*
+//array para remover el último de los items de "fruits"
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop();
+console.log(fruits);
+*/
+
+/*
+//Use the correct Array method to add "Kiwi" to the fruits array.
+const fruits = ["Banana", "Orange", "Apple"];
+fruits.push("Kiwi");
+console.log(fruits);
+*/
+
+//Use the splice() method to remove "Orange" and "Apple" from fruits.
+
+/*
+const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.splice(1,2);
+console.log(fruits);
+*/
+
+
+//Use the correct Array method to sort the fruits array alphabetically.
+const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.sort();
+console.log(fruits);
